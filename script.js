47c96969d5ae8b0bc317badb4ca6dfc0b9e57ee1
avongalie <alexsiaavila@gmail.com>
2022-03-31 18:44:17 -0400

    implemented hamburger menu

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-const cocktailMenu = document.getElementById('cocktail-menu');
-const dinnerMenu = document.getElementById('dinner-menu');
+const hambgMenu = document.getElementById('hambg-menu');
+const logo = document.getElementById('logo');
+const ul = document.querySelector('ul');
+const mainContent = document.getElementById('main-content');
+const menuOverlay = document.getElementById('menu-overlay');
 
-let menuActive = "dinner";
+const navLink = document.querySelectorAll(".nav-link");
 
-//hides cocktail menu on intialization
-cocktailMenu.style.display = "none";
+const hm = document.getElementById('hm');
 
 
-function openCocktailMenu(){
-    dinnerMenu.style.display = "none";
-    cocktailMenu.style.display = "block";
-}
+let menuOpen = false;
 
-function openDinnerMenu(){
-    cocktailMenu.style.display = "none";
-        dinnerMenu.style.display = "block";
-}
+hm.addEventListener('click', (event) => {
+    if(event.target.id === "hambg-menu"){
+        if(menuOpen === true){
+            hambgMenu.style.color = "white";
+            navLink.forEach((item) =>{
+                // item.style.color = "white";
+                item.classList.add("white");
+                item.classList.remove("black");
+            }
+            )
+            ul.classList.add("hidden");
+            logo.style.zIndex = "1";
+            mainContent.style.display = "block";
+            menuOverlay.style.display = "none";
+            menuOpen = false;
+        }else{
+            hambgMenu.style.color = "black";
+            navLink.forEach((item) =>{
+                // item.style.color = "black";
+                
+                item.classList.remove("white");
+                item.classList.add("black");
+            }
+            )
+            ul.classList.remove("hidden");
+            logo.style.zIndex = "0";
+            mainContent.style.display = "none";
+            menuOverlay.style.display = "block";
+            menuOpen = true;
+        }
+    }
+})
